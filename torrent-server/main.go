2952687ebc2fa6c176e94d68605873fbdb79d458
Fre576 <57255040+Fre576@users.noreply.github.com>
2026-06-21 19:53:02 +0200

    Extend playback limiter active window

--- torrent-server/main.go
+++ torrent-server/main.go
@@ -41,7 +41,7 @@ func (p *PlaybackLimiter) Start(client *bittorrent.Client) {
 func (p *PlaybackLimiter) MarkActive(client *bittorrent.Client, infoHash string) {
 	p.mu.Lock()
 	p.activeInfoHash = strings.ToLower(infoHash)
-	p.activeUntil = time.Now().Add(5 * time.Minute)
+	p.activeUntil = time.Now().Add(30 * time.Minute)
 	p.mu.Unlock()
 
 	p.ApplyPolicy(client)
